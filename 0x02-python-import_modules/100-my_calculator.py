af0bfc8f7b0d60b1e3d097f4de89559ce91fe907
globalsmile <mohammedbadmus823@gmail.com>
2023-06-08 21:34:03 -0700

    modified 100-my_calculator.py file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    args = len(sys.argv)
-    if(args != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    operator = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if(operator in "+-*/"):
-        if operator is "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator is "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator is "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    """
+    Handles basic operations
+
+    Performs basic operations like addition, substraction,
+    multiplication and division between two numbers.
+
+    The program will execute an operation between two numbers
+    selected by the operator sent to the program.
+    """
+    l_av = len(argv) - 1
+
+    if l_av == 3:
+        operator = argv[2]
+        num_a = int(argv[1])
+        num_b = int(argv[3])
+        if operator == '+':
+            res = add(num_a, num_b)
+            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '-':
+            res = sub(num_a, num_b)
+            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '*':
+            res = mul(num_a, num_b)
+            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '/':
+            res = div(num_a, num_b)
+            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
         else:
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
