7ce0ebed310578bdb254b2670bb83791b0e52346
globalsmile <mohammedbadmus823@gmail.com>
2023-06-27 18:01:57 -0700

    modified all .py file to meet requirements

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
-
-"""
-
-
 class Square:
-
+    """
+    creates a square class object
+    """
     def __init__(self, size):
-        """__init__
-
-        The __init__ method initializes the size value
-        of the square.
-
-        Attributes:
-            size (int): The size of the square.
-
-        """
         self.__size = size
+    """
+    initializes a square object with parameters
+    args:
+        __size(int): size of the square
+    """
