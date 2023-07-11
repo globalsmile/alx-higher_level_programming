ef98ef4a7de4df41aeb1f739fccc19d964de4f64
globalsmile <mohammedbadmus823@gmail.com>
2023-07-11 15:21:08 -0700

    modified 6-load_from_json_file.py and 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """loads adds saves json to file"""
 import json
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     jsonList = load_from_json_file("add_item.json")
