9dd18aed77db714b214a79c57564a4419f828aa3
globalsmile <mohammedbadmus823@gmail.com>
2023-09-28 16:37:22 -0700

    modified: 6-peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-# finds peak
+"""
+finds peak
+"""
 
 
 def search(lo, h, ints):
+    """
+    search for the peak of integers.
+    """
+
     mid = (lo + h) // 2
     if lo == h:
         return ints[h]
@@ -12,6 +18,10 @@ def search(lo, h, ints):
 
 
 def find_peak(list_of_integers):
+    """
+    find the peak of the integers using the search option above.
+    """
+
     if not list_of_integers:
         return
     return(search(0, len(list_of_integers) - 1, list_of_integers))
